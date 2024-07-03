5ebcbabd9d1b6d6c8a4f722300113510eca47fba
m4tt18 <mateus.s18@hotmail.com>
2024-07-03 16:31:32 -0300

    Commit - Criação das variáveis e métodos da classe Gerador.

--- src/main/java/org/example/entities/Gerador.java
+++ src/main/java/org/example/entities/Gerador.java
@@ -1,4 +1,30 @@
 package org.example.entities;
 
+import java.security.SecureRandom;
+
 public class Gerador {
+
+    private static final String CHARACTERS_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHARACTERS_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String CHARACTERS_NUMBERS = "0123456789";
+    private static final String CHARACTERS_SPECIAL = "!@#$%&*<>?";
+
+    public static String passwordGenerator(
+            int lenght, boolean uppercaseInclusion, boolean specialInclusion) {
+        String characters = CHARACTERS_LOWERCASE + CHARACTERS_NUMBERS;
+        if (uppercaseInclusion) {
+            characters += CHARACTERS_UPPERCASE;
+        } else if (specialInclusion) {
+            characters += CHARACTERS_SPECIAL;
+        }
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < lenght; i++) {
+            int index = random.nextInt(characters.length());
+            password.append(characters.charAt(index));
+        }
+        return password.toString();
+    }
 }
